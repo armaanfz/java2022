29dfbd503427a3ed9b91fda5b79a7e7b45083581
armaanfz <armaanf@my.ofs.edu.sg>
2022-09-29 09:50:08 +0800

    update

--- Example4.java
+++ Example4.java
@@ -4,15 +4,12 @@ Chapter 1, example 4: for loop
 
 */
 
-public class Example4
-{
-	public static void main(String[] args)
-	{
-		for(int i = 0; i < 20; i++)
+public class Example4 {
+	public static void main(String[] args) {
+		for(int i = 1; i <= 20; i++)
 		{	// everything in between these { } will repeat 20 times
 			IBIO.output("Hello " + i);
 		}
-		
 		/*
 		// this while loop does the same as the for loop above; the for loop abbreviates the while loop structure below:
 		int i = 0;
