23635e0cf099a2d1b0b410a59aff0f19f12d9c2f
armaanfz <armaanf@my.ofs.edu.sg>
2022-10-01 18:32:33 +0800

    Mock Q3 Done

--- Mock.java
+++ Mock.java
@@ -1,12 +1,22 @@
 public class Mock {
     public static void main(String[] args) {
         System.out.println("My name is Armaan");
-        int n = IBIO.inputInt("Input any number: ");
-        if (n > 0) {
+        int n = IBIO.inputInt("Calculate up to term (n) ? ");
+        /* if (n > 0) {
             IBIO.output("The number was " + n);
         }
         else {
             IBIO.output("Error - enter a positive number");
+        }
+        */
+        int num1 = 0, num2 = 1;
+        int counter = 0;
+        while (counter < n) {
+            System.out.print(num1 + "; ");
+            int num3 = num2 + num1;
+            num1 = num2;
+            num2 = num3;
+            counter = counter + 1;
         }
     }
 }
