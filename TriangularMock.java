76b0fad541e44c0c7fdc9c469ce635cc51448a6a
armaanfz <armaanf@my.ofs.edu.sg>
2022-10-07 09:27:55 +0800

    Tri Mock Done

--- TriangularMock.java
+++ TriangularMock.java
@@ -1,9 +1,18 @@
 public class TriangularMock {
     public static void main(String[] args) {
         IBIO.out("Darth Vader");
-        int n = -1;
+        /* int n = -1;
         while(n < 0) {
             n = IBIO.inputInt("\nCalculate up to term (n) ? ");
+        } */
+        int n = IBIO.inputInt("\nCalculate up to term (n) ? ");
+        int counter = 0;
+        int num1 = 0;
+        int num2 = 1;
+        while (counter < n) {
+            int num3 = num1 + num2;
+            num3 +
+            counter++;
         }
     }
 }
