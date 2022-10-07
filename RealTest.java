501b44a5b9da3721809bb85f591e7ef07b4f0b64
armaanfz <armaanf@my.ofs.edu.sg>
2022-10-07 10:51:12 +0800

    RealTest Done

--- RealTest.java
+++ RealTest.java
@@ -8,24 +8,40 @@ public class RealTest {
         }
 
         int product;
-        for(int i = 1; i <= 20; i++) {
-            product = i *  n;
-            if (i <= 9) {
-                if (product < 10) {
-                    IBIO.output(n + " x  " + i + " =   " + product);
-                }
-                else {
-                    IBIO.output(n + " x  " + i + " =  " + product);
+        while(n != 0) {
+            for(int j = 1; j <= n; j++) {
+                for(int i = 1; i <= 20; i++) {
+                    product = j * i;
+                    if (i == 20) {
+                        if (product < 100) {
+                            IBIO.output(j + " x " + i + " =  " + product);
+                        }
+                        else {
+                            IBIO.output(j + " x " + i + " = " + product);
+                        }
+                        IBIO.output("------------");
+                    }
+                    else {
+                        if (i <= 9) {
+                            if (product < 10) {
+                                IBIO.output(j + " x  " + i + " =   " + product);
+                            }
+                            else {
+                                IBIO.output(j + " x  " + i + " =  " + product);
+                            }
+                        }
+                        else {
+                            if (product < 100) {
+                                IBIO.output(j + " x " + i + " =  " + product);
+                            }
+                            else {
+                                IBIO.output(j + " x " + i + " = " + product);
+                            }
+                        }
+                    }
                 }
             }
-            else {
-                if (product < 100) {
-                    IBIO.output(n + " x " + i + " =  " + product);
-                }
-                else {
-                    IBIO.output(n + " x " + i + " = " + product);
-                }
-            }
+            n = IBIO.inputInt("Enter an integer between 1 and 9: ");
         }
     }
 }
