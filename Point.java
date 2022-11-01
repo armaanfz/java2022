9348d02cbb07c2c786a922073e435652d9e354f1
armaanfz <armaanf@my.ofs.edu.sg>
2022-11-01 18:38:06 +0800

    Point HW done

--- Point.java
+++ Point.java
@@ -1,4 +1,3 @@
-
 public class Point
 {
 	char name;
@@ -20,22 +19,21 @@ public class Point
 	
 	public double distance(Point p2)
 	{
-		if ((this.x - p2.x)/(this.y - p2.y) >= 0) {
+		if ((this.x - p2.x) / (this.y - p2.y) >= 0) {
 			return (this.x - p2.x) / (this.y - p2.y);
 		}
 		else {
-			return -(this.x - p2.x) / (this.y - p2.y);
+			return -((this.x - p2.x) / (this.y - p2.y));
 		}
 	}
 	
 	public double OriginDistance() // distance from 0,0
 	{
-		if ((0 - this.x)/(0 - this.y) >= 0) {
-			return ((0 - this.x)
-					/(0 - this.y));
+		if ((0 - this.x) / (0 - this.y) >= 0) {
+			return (0 - this.x) / (0 - this.y);
 		}
 		else {
-			return -((0 - this.x)/(0 - this.y));
+			return -((0 - this.x) / (0 - this.y));
 		}
 	}
 
@@ -49,9 +47,11 @@ public class Point
 		p.printPoint();
 		Point q = new Point('Q', IBIO.inputDouble("X-coordinate of Q: "), IBIO.inputDouble("Y-coordinate of Q: "));
 		q.printPoint();
+		Point r = new Point('Q', IBIO.inputDouble("X-coordinate of Q: "), IBIO.inputDouble("Y-coordinate of Q: "));
+		r.printPoint();
 		System.out.println( "P=Q? " + p.equals(q));
+		System.out.println( "P=R? " + p.equals(r));
 		System.out.println( "Distance between P and Q = " + p.distance(q));
 		System.out.println( "Distance of P to origin (0,0) = " + p.OriginDistance());
 	}
 }
-
