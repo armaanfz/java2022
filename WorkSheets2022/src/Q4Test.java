347229dace92b35cca42746edf08f10503791218
armaanfz <armaanf@my.ofs.edu.sg>
2023-04-26 14:55:00 +0800

    Q4Test done

--- WorkSheets2022/src/Q4Test.java
+++ WorkSheets2022/src/Q4Test.java
@@ -80,7 +80,7 @@ public class Q4Test
         int temp;
         for (int i = 0; i < a.length; i++) {
             for (int j = 0; j < a.length - i - 1; j++) {
-                if (a[j] > a[j + 1]) {
+                if (a[j] < a[j + 1]) {
                     temp = a[j];
                     a[j] = a[j + 1];
                     a[j + 1] = temp;
