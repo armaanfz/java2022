03264459fe7143da75a893ce091efb2434481b03
armaanfz <armaanf@my.ofs.edu.sg>
2023-04-26 14:51:40 +0800

    Q4Test done

--- WorkSheets2022/src/Q4Test.java
+++ WorkSheets2022/src/Q4Test.java
@@ -1,61 +1,128 @@
-import java.util.Stack;
-
 public class Q4Test
 {
     public static int[] clone(int[] a)
     {
-        return null; // change...
+        int[] x = new int[a.length];
+        //noinspection ManualArrayCopy
+        for (int i = 0; i < a.length; i++) {
+            x[i] = a[i];
+        }
+        return x;
     }
 
     public static String[] clone(String[] a)
     {
-        return null; // change...
+        String[] x = new String[a.length];
+        //noinspection ManualArrayCopy
+        for (int i = 0; i < a.length; i++) {
+            x[i] = a[i];
+        }
+        return x;
     }
 
     public static void printArray(String[] array)
     {
-
+        for(int i = 0; i < array.length; i++) {
+            System.out.print( "[" + i + "]" + array[i] + "  ");
+        }
+        System.out.println();
     }
 
     public static void printArray(int[] array)
     {
-
+        for(int i = 0; i < array.length; i++) {
+            System.out.printf( "[%d]%-2d  ",i , array[i]);
+        }
+        System.out.println();
     }
 
     public static void parallelPrint(String[] sa, int[] ia)
     {
-
+        for(int i = 0; i < sa.length; i++) {
+            System.out.print( "[" + i + "]" + sa[i]);
+            System.out.println("    " + ia[i]);
+        }
+        System.out.println();
     }
 
-    // true if arrays may be parallel, false otherwise
     public static boolean areParallel(String[] sa, int[] ia)
     {
-        return false; // change...
+        return sa.length == ia.length;
     }
 
     public static void search(String[] sa, int[] ia, int goals)
     {
-
+        for (int i = 0; i < ia.length; i++) {
+            if (ia[i] > goals) {
+                System.out.print(sa[i]);
+                System.out.println("    " + ia[i]);
+            }
+        }
     }
 
     public static void selectionSort(String[] sa)
     {
-
+        for (int i = 0; i < sa.length -1; i++) {
+            int smallestIndex = i;
+            for (int j = i + 1; j < sa.length; j++) {
+                if ((sa[j].compareTo(sa[smallestIndex])) < 0) {
+                    smallestIndex = j;
+                }
+            }
+            String temp = sa[smallestIndex];
+            sa[smallestIndex] = sa[i];
+            sa[i] = temp;
+        }
     }
 
-    public static void bubbleSort(int[] array)
+    public static void bubbleSort(int[] a)
     {
-
+        int temp;
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length - i - 1; j++) {
+                if (a[j] > a[j + 1]) {
+                    temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+            }
+        }
     }
 
     public static void parallelSelectionSort(String[] sa, int[] ia)
     {
-
+        for (int i = 0; i < sa.length -1; i++) {
+            int smallestIndex = i;
+            for (int j = i + 1; j < sa.length; j++) {
+                if ((sa[j].compareTo(sa[smallestIndex])) < 0) {
+                    smallestIndex = j;
+                }
+            }
+            String temp = sa[smallestIndex];
+            int temp2 = ia[smallestIndex];
+            sa[smallestIndex] = sa[i];
+            ia[smallestIndex] = ia[i];
+            sa[i] = temp;
+            ia[i] = temp2;
+        }
     }
 
     public static void parallelBubbleSort(String[] sa, int[] ia)
     {
-
+        int temp;
+        String temp2;
+        for (int i = 0; i < ia.length; i++) {
+            for (int j = 0; j < ia.length - i - 1; j++) {
+                if (ia[j] < ia[j + 1]) {
+                    temp = ia[j];
+                    temp2 = sa[j];
+                    ia[j] = ia[j + 1];
+                    sa[j] = sa[j + 1];
+                    ia[j + 1] = temp;
+                    sa[j + 1] = temp2;
+                }
+            }
+        }
     }
 
 
@@ -97,7 +164,7 @@ public class Q4Test
         parallelPrint(players2, records2);
         System.out.println("\nSort parallel arrays by player names with selection sort");
         parallelSelectionSort(players3, records3); // 10
-        System.out.println("Players by name");
+        System.out.println("\nPlayers by name");
         parallelPrint(players3, records3);
         // +: beautify the output
     }
